e8a0c0d0597bc31f6adffcf172512e051bfbaf06
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-06-13 17:27:41 +0300

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_start_page),
+    path('', views.get_geo_json),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,53 @@
+import json
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from places.models import Place, Image
 
 
-def show_start_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+# def show_start_page(request):
+#     template = loader.get_template('index.html')
+#     context = {}
+#     rendered_page = template.render(context, request)
+#     return HttpResponse(rendered_page)
+
+
+def get_geo_json(request):
+    places = Place.objects.all()
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        imgs = []
+        for image in place.images.all():
+            imgs.append(str(image.image))
+        features = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.coordinate_lng, place.coordinate_lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": 'static/places/moscow_legends.json',
+                # "detailsUrl": {
+                #     "title": place.title,
+                #     "imgs": imgs,
+                #     "description_short": place.description_short,
+                #     "description_long": place.description_long,
+                #         "coordinates": {
+                #         "lng": place.coordinate_lng,
+                #         "lat": place.coordinate_lat
+                #     }
+                # }
+            }
+        }
+        geo_json['features'].append(features)
+    places_geo_json = {
+        'places': geo_json
+    }
+    return render(request, "index.html", context=places_geo_json)
+
+
