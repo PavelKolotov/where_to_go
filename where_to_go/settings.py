409f05d9157e5a8e99e6a1c2f8df8c77e6b1e291
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-06-15 16:51:06 +0300

    added environment variable

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,6 +13,13 @@ import os
 
 from pathlib import Path
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z-j@d^8mpbto@=+i-zf9gk19*29&n$ch+bs(5-oi115r@i51=k'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
