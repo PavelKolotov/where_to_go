b4dea10e4d6095858f95443ca89f4823a9aba83b
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-06-19 16:10:49 +0300

    fix place model

--- places/models.py
+++ places/models.py
@@ -3,11 +3,11 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Заголовок', max_length=100, blank=True, unique=True)
+    title = models.CharField('Заголовок', max_length=100, unique=True)
     description_short = models.TextField('Краткое описание', blank=True)
     description_long = HTMLField('Описание', blank=True)
-    coordinate_lng = models.FloatField('Долгота', null=True)
-    coordinate_lat = models.FloatField('Широта', null=True)
+    coordinate_lng = models.FloatField('Долгота')
+    coordinate_lat = models.FloatField('Широта')
 
     def __str__(self):
         return self.title
